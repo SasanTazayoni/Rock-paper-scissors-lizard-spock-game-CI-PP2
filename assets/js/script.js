2f6b478195f8bc931721b4bb76b964a3825aaf5a
Sasan Tazayoni <sasantazayoni@gmail.com>
2023-08-15 16:30:53 +0000

    Add JS to main UI to navigate to rules modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,18 @@
 const inititalModal = document.querySelector('[data-modal]');
 const overlay = document.querySelector('[data-overlay]');
 const playGameBtn = document.querySelectorAll('[data-play-btn]');
+const openRulesModalBtn = document.querySelectorAll('[data-rules-btn]');
 
 playGameBtn.forEach(btn => {
     btn.addEventListener('click', () => {
-        overlay.classList.toggle('open');
-        inititalModal.classList.toggle('open');
+        overlay.classList.remove('open');
+        inititalModal.classList.remove('open');
+    });
+});
+
+openRulesModalBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        overlay.classList.add('open');
+        inititalModal.classList.add('open');
     });
 });
