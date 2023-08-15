a6bbfa939e4366f4a44805b58f396329fd0f26d0
Sasan Tazayoni <sasantazayoni@gmail.com>
2023-08-15 17:27:23 +0000

    Add JS functionality for opening the game score tab

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ const inititalModal = document.querySelector('[data-modal]');
 const overlay = document.querySelector('[data-overlay]');
 const playGameBtn = document.querySelectorAll('[data-play-btn]');
 const openRulesModalBtn = document.querySelectorAll('[data-rules-btn]');
+const checkScoresBtn = document.querySelector('[data-scores-btn]');
+const scoresModal = document.querySelector('[data-scores-modal]');
 
 playGameBtn.forEach(btn => btn.addEventListener('click', closeModal));
 overlay.addEventListener('click', closeModal);
@@ -17,3 +19,8 @@ function closeModal() {
     inititalModal.classList.remove('open');
     overlay.classList.remove('open');
 }
+
+checkScoresBtn.addEventListener('click', () => {
+    scoresModal.classList.add('open');
+    overlay.classList.add('open');
+});
