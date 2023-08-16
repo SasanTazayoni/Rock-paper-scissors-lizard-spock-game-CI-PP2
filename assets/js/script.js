441fd8494d0081cfad087d3ddd65058faa80eb3d
Sasan Tazayoni <sasantazayoni@gmail.com>
2023-08-16 14:53:08 +0000

    Add JS code for player selection vs computer selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,40 @@ const playGameBtn = document.querySelectorAll('[data-play-btn]');
 const openRulesModalBtn = document.querySelectorAll('[data-rules-btn]');
 const checkScoresBtn = document.querySelector('[data-scores-btn]');
 const scoresModal = document.querySelector('[data-scores-modal]');
+const selectionIcons = document.querySelectorAll('[data-selection]');
+let playerScore = document.querySelector('[data-player-score]');
+let computerScore = document.querySelector('[data-computer-score]');
+let win = document.querySelector('[data-player-wins]');
+let lose = document.querySelector('[data-computer-wins]');
+const rules = [
+    {
+        name: 'rock',
+        symbol: '✊',
+        beats: ['scissors', 'lizard']
+    },
+    {
+        name: 'paper',
+        symbol: '🖐',
+        beats: ['rock', 'spock']
+    },
+    {
+        name: 'scissors',
+        symbol: '✌️',
+        beats: ['paper', 'lizard']
+    },
+    {
+        name: 'lizard',
+        symbol: '🤏',
+        beats: ['spock', 'paper']
+    },
+    {
+        name: 'spock',
+        symbol: '🖖',
+        beats: ['scissors', 'rock']
+    }
+]
+
+// Opening and closing the modals
 
 playGameBtn.forEach(btn => btn.addEventListener('click', closeModal));
 overlay.addEventListener('click', closeModal);
@@ -25,3 +59,33 @@ checkScoresBtn.addEventListener('click', () => {
     scoresModal.classList.add('open');
     overlay.classList.add('open');
 });
+
+// Game functionality
+
+function computerPlay() {
+    return rules[Math.floor(Math.random() * rules.length - 1)][0];
+}
+
+selectionIcons.forEach(icon => {
+    icon.addEventListener('click', () => {
+        const computerResult = computerPlay();
+        const selectedIcon = icon.dataset.selection;
+        const playerResult = rules.find(rule => rule.name === selectedIcon);
+        const gameResult = playRound(playerResult, computerResult);
+        game(gameResult);
+    });
+});
+
+function playRound(playerChoice, computerChoice) {
+    const playerWin = determineWinner(playerChoice, computerChoice);
+    const computerWin = determineWinner(computerChoice, playerChoice);
+    updateUI(computerChoice, computerWin);
+    updateUI(playerChoice, playerWin);
+    if (playerWin) {
+        return 'WIN';
+    } else if (computerWin) {
+        return 'LOSE';
+    } else {
+        return 'DRAW';
+    }
+}
