8a5b60ccb04f968f9fd83541b2c382fc7bd5b97b
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-07-16 10:36:17 +0100

    Create a separate component for footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        &copy; Rock Paper Scissors Lizard Spock game 2025 created by Sasan
+        Tazayoni
+      </p>
+      <a
+        href="https://github.com/SasanTazayoni"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="github-link"
+        aria-label="Github profile link"
+      >
+        <i className="fa-brands fa-github"></i>
+      </a>
+    </footer>
+  );
+}
