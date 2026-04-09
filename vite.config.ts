b7fdd1f78101d7fc6700c39cc7121cc934bf091b
Sasan Tazayoni <sasantazayoni@gmail.com>
2026-04-09 14:39:57 +0100

    Update vite config import and fix BASE_URL for new repo

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Rock-paper-scissors-lizard-spock-game-CI-PP2/",
+  base: "/Rock-paper-scissors-lizard-spock/",
   plugins: [react()],
   test: {
     environment: "jsdom",
